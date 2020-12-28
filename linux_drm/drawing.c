8abe7c37b471aec6f39956a3619f0e954c170350
Taylon Silva <taylonsilva@gmail.com>
2020-12-27 19:03:05 -0800

    linux drm

--- linux_drm/drawing.c
+++ linux_drm/drawing.c
@@ -1,10 +1,8 @@
-#define _GNU_SOURCE
 #include <stdint.h>
 #include <stdbool.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include <unistd.h>
 #include <fcntl.h>
 #include <sys/mman.h>
 #include <errno.h>
@@ -88,86 +86,105 @@ int create_framebuffer(int video_card_fd, FrameBuffer *framebuffer) {
   return 0;
 }
 
-int main(int argc, char *argv[]) {
-  int video_card_fd = open("/dev/dri/card0", O_RDWR);
-  if (video_card_fd < 0) {
-    fprintf(stderr, "unable to open /dev/dri/card0: %s\n", strerror(errno));
-    exit(EXIT_FAILURE);
-  }
-
-  drmModeRes *resources = drmModeGetResources(video_card_fd);
-  if (resources == NULL) {
-    fprintf(stderr, "unable to retrieve DRM resources %s\n", strerror(errno));
-    drmClose(video_card_fd);
-    exit(EXIT_FAILURE);
-  }
-
+drmModeConnector *find_connector(int video_card_fd, drmModeRes *resources) {
   // find the first available connector with modes
-  drmModeConnector *connector = NULL;
+  drmModeConnector *connector;
   for (int i = 0; i < resources->count_connectors; ++i) {
     connector = drmModeGetConnector(video_card_fd, resources->connectors[i]);
     if (connector != NULL) {
       // check if the monitor is connected and has at least one valid mode
       if (connector->connection == DRM_MODE_CONNECTED &&
           connector->count_modes > 0) {
-        printf("Connector found: %d\n", connector->connector_id);
-        break;
+        return connector;
       }
 
-      connector = NULL;
       drmModeFreeConnector(connector);
     }
   }
 
+  return NULL;
+}
+
+drmModeEncoder *find_encoder_by_id(int video_card_fd, drmModeRes *resources,
+                                   uint32_t encoder_id) {
+  drmModeEncoder *encoder;
+  for (int i = 0; i < resources->count_encoders; ++i) {
+    encoder = drmModeGetEncoder(video_card_fd, resources->encoders[i]);
+    if (encoder != NULL) {
+      if (encoder->encoder_id == encoder_id) {
+        fprintf(stderr, "Encoder found: %d\n", encoder->encoder_id);
+        return encoder;
+      }
+
+      drmModeFreeEncoder(encoder);
+    }
+  }
+
+  return NULL;
+}
+
+void render(FrameBuffer *framebuffer) {
+  int rect_width = 400;
+  int rect_height = 200;
+
+  for (int vertical = (framebuffer->height / 2) - (rect_height / 2);
+       vertical < ((framebuffer->height / 2) + (rect_height / 2)); ++vertical) {
+    for (int horizontal = ((framebuffer->width / 2) - rect_width);
+         horizontal < ((framebuffer->width / 2) + rect_width); ++horizontal) {
+      int offset = (framebuffer->pitch * vertical) +
+                   (horizontal * (framebuffer->bytes_per_pixel / 8));
+      *(uint32_t *)&framebuffer->pixels[offset] =
+          (255 << 16) | (255 << 8) | 255;
+    }
+  }
+}
+
+int main(int argc, char *argv[]) {
+  int video_card_fd = open("/dev/dri/card0", O_RDWR);
+  if (video_card_fd < 0) {
+    fprintf(stderr, "unable to open /dev/dri/card0: %s\n", strerror(errno));
+    return EXIT_FAILURE;
+  }
+
+  drmModeRes *resources = drmModeGetResources(video_card_fd);
+  if (resources == NULL) {
+    fprintf(stderr, "unable to retrieve DRM resources %s\n", strerror(errno));
+    return EXIT_FAILURE;
+  }
+
+  drmModeConnector *connector = find_connector(video_card_fd, resources);
   if (connector == NULL) {
     fprintf(stderr, "no valid connector found\n");
-    drmModeFreeResources(resources);
-    exit(EXIT_FAILURE);
+    return EXIT_FAILURE;
   }
 
   drmModeModeInfo mode = connector->modes[1];
   fprintf(stderr, "Mode: %dx%d - %dhz\n", mode.hdisplay, mode.vdisplay,
           mode.vrefresh);
   // prints all modes
-  /* for (int i = 0; i < connector->count_modes; i++) { */
-  /*   drmModeModeInfo mode = connector->modes[i]; */
-  /*   printf("(%dx%d@%dhz)\n", mode.hdisplay, mode.vdisplay, */
-  /*           mode.vrefresh); */
-  /* } */
+  // for (int i = 0; i < connector->count_modes; i++) {
+  //  drmModeModeInfo mode = connector->modes[i];
+  //  printf("(%dx%d@%dhz)\n", mode.hdisplay, mode.vdisplay,
+  //          mode.vrefresh);
+  // }
 
-  // find the encoder matching the connector we found previously
-  drmModeEncoder *encoder = NULL;
-  for (int i = 0; i < resources->count_encoders; ++i) {
-    encoder = drmModeGetEncoder(video_card_fd, resources->encoders[i]);
-    if (encoder != NULL) {
-      fprintf(stderr, "Encoder found: %d\n", encoder->encoder_id);
-
-      if (encoder->encoder_id == connector->encoder_id)
-        break;
-
-      drmModeFreeEncoder(encoder);
-    }
-  }
-
+  drmModeEncoder *encoder =
+      find_encoder_by_id(video_card_fd, resources, connector->encoder_id);
   if (encoder == NULL) {
     fprintf(stderr, "No enconder matching with connector\n");
-    drmModeFreeResources(resources);
-    exit(EXIT_FAILURE);
+    return EXIT_FAILURE;
   }
 
   // alloc two buffers so we can flip between them when rendering
-  FrameBuffer *framebuffers = calloc(2, sizeof(FrameBuffer));
-  framebuffers[0].height = mode.vdisplay;
-  framebuffers[1].height = mode.vdisplay;
-  framebuffers[0].width = mode.hdisplay;
-  framebuffers[1].width = mode.hdisplay;
+  FrameBuffer framebuffers[2] = {
+      {.width = mode.hdisplay, .height = mode.vdisplay},
+      {.width = mode.hdisplay, .height = mode.vdisplay}};
   int err = create_framebuffer(video_card_fd, &framebuffers[0]);
   int err_2 = create_framebuffer(video_card_fd, &framebuffers[1]);
   if (err || err_2) {
     fprintf(stderr, "unable to create framebuffers (%d): %m\n", errno);
-    exit(EXIT_FAILURE);
+    return EXIT_FAILURE;
   }
-  int framebuffer_index = 0;
 
   // modeset
   // we save the crtc before we change it, that way we can restore
@@ -177,49 +194,34 @@ int main(int argc, char *argv[]) {
     fprintf(stderr, "unable not retrieve information about the crtc: %s\n",
             strerror(errno));
 
-    exit(EXIT_FAILURE);
+    return EXIT_FAILURE;
   }
 
-  err = drmModeSetCrtc(video_card_fd, encoder->crtc_id,
-                       framebuffers[framebuffer_index].id, 0, 0,
+  int front_framebuffer_index = 0;
+  err = drmModeSetCrtc(video_card_fd, crtc->crtc_id,
+                       framebuffers[front_framebuffer_index].id, 0, 0,
                        &connector->connector_id,
                        1, // element count of the connectors array above
                        &mode);
   if (err) {
     fprintf(stderr, "modesetting failed: %s\n", strerror(errno));
-    drmModeRmFB(video_card_fd, framebuffers[framebuffer_index].id);
-    exit(EXIT_FAILURE);
+    drmModeRmFB(video_card_fd, framebuffers[front_framebuffer_index].id);
+    return EXIT_FAILURE;
   }
 
-  int rect_width = 400;
-  int rect_height = 200;
-
-  FrameBuffer *framebuffer;
-  for (int vertical_position = 0; vertical_position < (mode.vdisplay - 200);
-       vertical_position += 20) {
-    framebuffer = &framebuffers[framebuffer_index ^ 1];
-
+  bool running = true;
+  while (running) {
+    FrameBuffer *framebuffer = &framebuffers[front_framebuffer_index ^ 1];
     memset(framebuffer->pixels, 1, framebuffer->size);
 
-    for (int vertical = vertical_position;
-         vertical < (vertical_position + rect_height); ++vertical) {
-      for (int horizontal = ((framebuffer->width / 2) - rect_width);
-           horizontal < ((framebuffer->width / 2) + rect_width); ++horizontal) {
-        int offset = (framebuffer->pitch * vertical) +
-                     (horizontal * (framebuffer->bytes_per_pixel / 8));
-        *(uint32_t *)&framebuffer->pixels[offset] =
-            (255 << 16) | (255 << 8) | 255;
-      }
-    }
+    render(framebuffer);
 
-    err = drmModeSetCrtc(video_card_fd, encoder->crtc_id, framebuffer->id, 0, 0,
+    err = drmModeSetCrtc(video_card_fd, crtc->crtc_id, framebuffer->id, 0, 0,
                          &connector->connector_id, 1, &mode);
     if (err)
       fprintf(stderr, "unable to flip buffers (%d): %m\n", errno);
     else
-      framebuffer_index ^= 1;
-
-    usleep(100000);
+      front_framebuffer_index ^= 1;
   }
 
   // cleanup
@@ -228,5 +230,5 @@ int main(int argc, char *argv[]) {
                  crtc->y, &connector->connector_id, 1, &crtc->mode);
   drmModeFreeCrtc(crtc);
 
-  exit(EXIT_SUCCESS);
+  return EXIT_SUCCESS;
 }
